81a0300cadb75411483afcae75a5e090ab8ec33f
kida <leslug.away@gmail.com>
2023-09-16 21:27:11 +0800

    Fixed unable to load images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['be.sollinked.com', 'api.sollinked.com', 'localhost'],
+        domains: ['be.sollinked.com', 'api.sollinked.com', 'localhost', 'v2.sollinked.com', 'app.sollinked.com'],
     },
 }
 
