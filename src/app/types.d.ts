da6c7734d7b6edd6b9de7d9e9be19b6c83419686
kida <leslug.away@gmail.com>
2023-11-19 16:39:25 +0800

    Update typings

--- src/app/types.d.ts
+++ src/app/types.d.ts
@@ -1,7 +1,11 @@
+import { UserTag } from "../../types";
+
 export type HomepageUser = {
+    id: number;
     username: string;
     display_name: string;
     profile_picture?: string;
     value_usd: number;
     is_verified: boolean;
+    tags?: UserTag[];
 }
